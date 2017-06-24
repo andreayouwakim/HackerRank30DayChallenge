a95e8c18c1c8837669a59845f850934590d86f13
Andrea Youwakim <azy98.ayy@gmail.com>
2017-06-23 23:27:40 -0500

    Update Day7.java

--- Day7.java
+++ Day7.java
@@ -1,5 +1,49 @@
 /**
- * Created by andreayouwakim on 6/16/17.
+ * Objective 
+Today, we're learning about the Array data structure. 
+Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
+
+Input Format
+
+The first line contains an integer,  (the size of our array). 
+The second line contains  space-separated integers describing array 's elements.
+
+Constraints
+
+, where  is the  integer in the array.
+Output Format
+
+Print the elements of array  in reverse order as a single line of space-separated numbers.
+
+Sample Input
+
+4
+1 4 3 2
+Sample Output
+
+2 3 4 1
  */
-public class Day7 {
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        in.close();
+        for(int x = arr.length - 1; x >= 0; x--){
+            System.out.print(arr[x] + " ");
+        }
+    }
 }

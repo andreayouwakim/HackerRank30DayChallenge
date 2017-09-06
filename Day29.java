e85eabd23f65c89562c74bc871404e3812f28c5d
Andrea Youwakim <azy98.ayy@gmail.com>
2017-09-05 19:23:42 -0500

    Update Day29.java

--- Day29.java
+++ Day29.java
@@ -1,5 +1,65 @@
 /**
- * Created by andreayouwakim on 6/16/17.
+ * Objective
+ Link to problem : https://www.hackerrank.com/challenges/30-bitwise-and 
+Welcome to the last day! Today, we're discussing bitwise operations. 
+Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+Given set . Find two integers,  and  (where ), from set  such that the value of is 
+the maximum possible and also less than a given integer, . In this case,  represents the bitwise AND operator.
+
+Input Format
+
+The first line contains an integer, , the number of test cases. 
+Each of the  subsequent lines defines a test case as  space-separated integers,  and , respectively.
+
+Constraints
+
+Output Format
+
+For each test case, print the maximum possible value of  on a new line.
+
+Sample Input
+
+3
+5 2
+8 5
+2 2
+Sample Output
+
+1
+4
+0
+Explanation
+
+ 
+
+All possible values of  and  are:
+
+The maximum possible value of  that is also  is , so we print  on a new line.
  */
-public class Day29 {
+
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int i = 0; i < t; i++){
+            int n = in.nextInt();
+            int k = in.nextInt();
+            if(((k-1)|k) > n && k%2==0){
+                System.out.println(k-2);
+            }else{
+                System.out.println(k-1);               
+            }
+        }
+    }
 }
